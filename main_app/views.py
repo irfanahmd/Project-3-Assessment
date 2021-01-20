4baa48169a0bcb512e5a14a6a217771e7941c927
Irfan <irfan@NC-MBP.local>
2021-01-19 22:47:26 -0500

    updated wacky widgets

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Widget
+from django.views.generic.edit import CreateView
+from .forms import WidgetForm
+
 
 # Create your views here.
+
+
+def home(request):
+    widgets = Widget.objects.all()
+    form = WidgetForm()
+    return render(request, 'index.html', {'widgets': widgets, 'form': form})
+
+
+class WidgetCreate(CreateView):
+    model = Widget

b7d97368321371aead27b5b303dddf09d1ac7ced
Irfan <irfan@NC-MBP.local>
2021-01-19 23:21:40 -0500

    added bonus

--- main_app/views.py
+++ main_app/views.py
@@ -3,14 +3,16 @@ from .models import Widget
 from django.views.generic.edit import CreateView, DeleteView
 from .forms import WidgetForm
 
+from django.db.models import Sum
+
 
 # Create your views here.
-
 
 def home(request):
     widgets = Widget.objects.all()
     form = WidgetForm()
-    return render(request, 'index.html', {'widgets': widgets, 'form': form})
+    total_quantity = Widget.objects.all().aggregate(Sum('quantity'))
+    return render(request, 'index.html', {'widgets': widgets, 'form': form, 'total_quantity': total_quantity})
 
 
 class WidgetCreate(CreateView):
